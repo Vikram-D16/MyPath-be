f2ff8edbd7c1f3256226406d609c65e36f3716f3
Vikram-D16 <drvikram19@gmail.com>
2025-05-02 17:36:43 +0530

    initial run

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func main() {
 
     // Start the server on port 8080
     fmt.Println("Server is running on http://localhost:8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
+    if err := http.ListenAndServe(":8081", nil); err != nil {
         log.Fatal("Error starting the server:", err)
     }
 }
