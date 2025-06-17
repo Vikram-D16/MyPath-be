35763c72151d23da98f133e2f33dfaf0943c70c4
Vikram-D16 <drvikram19@gmail.com>
2025-06-18 01:46:43 +0530

    cors addition

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,26 +4,39 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/Vikram-D16/MyPath-be/db"
 	"github.com/Vikram-D16/MyPath-be/handlers"
 	"github.com/Vikram-D16/MyPath-be/middlewares"
 	"github.com/Vikram-D16/MyPath-be/models"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
 	db.InitDB()
 	dbInstance := db.DB
-
 	dbInstance.AutoMigrate(&models.User{})
 
 	router := gin.Default()
 
-	// Authenticate request
-	router.Use(func(c *gin.Context) {
-		if c.FullPath() != "/register" && c.FullPath() != "/login" {
+	// Add CORS middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	api := router.Group("/api")
+
+	// Auth middleware for protected routes
+	api.Use(func(c *gin.Context) {
+		if c.FullPath() != "/api/register" && c.FullPath() != "/api/login" {
 			middlewares.AuthMiddleware()(c)
 			if c.IsAborted() {
 				return
@@ -32,29 +45,27 @@ func main() {
 		c.Next()
 	})
 
-	// Define routes
-	router.POST("/register", handlers.RegisterHandler)
-	router.POST("/login", handlers.LoginHandler)
-	router.DELETE("/account", handlers.DeleteUserHandler)
+	api.POST("/register", handlers.RegisterHandler)
+	api.POST("/login", handlers.LoginHandler)
+	api.DELETE("/account", handlers.DeleteUserHandler)
 
-	router.GET("/home", handlers.HomeHandler)
-	router.GET("/users", handlers.GetAllUsersHandler)
+	api.GET("/home", handlers.HomeHandler)
+	api.GET("/users", handlers.GetAllUsersHandler)
 
-	router.POST("/organizations", handlers.CreateOrganizationHandler)
-	router.GET("/organizations", handlers.GetAllOrganizationsHandler)
+	api.POST("/organizations", handlers.CreateOrganizationHandler)
+	api.GET("/organizations", handlers.GetAllOrganizationsHandler)
 
-	router.POST("/groups", handlers.CreateGroupHandler)
-	router.GET("/groups", handlers.GetAllGroupsHandler)
-	router.DELETE("/groups/:id", handlers.DeleteGroupHandler)
+	api.POST("/groups", handlers.CreateGroupHandler)
+	api.GET("/groups", handlers.GetAllGroupsHandler)
+	api.DELETE("/groups/:id", handlers.DeleteGroupHandler)
 
-	router.POST("/group-member", handlers.CreateGroupMemberHandler)
+	api.POST("/group-member", handlers.CreateGroupMemberHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8081"
 	}
 
-	// Start the HTTP server
 	fmt.Printf("Server running on http://localhost:%s\n", port)
 	if err := router.Run(":" + port); err != nil {
 		log.Fatal(err)
